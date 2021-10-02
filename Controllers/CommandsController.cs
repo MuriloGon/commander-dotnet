2535cc90b18b3e54d243ef96db18a1c387413174
Murilo Gonçalves <murilo.gon@live.com>
2021-10-02 15:09:14 -0300

    feat: implement post method - create command

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -14,27 +14,46 @@ namespace Commander.Controllers
     private readonly ICommanderRepo _Repository;
     private readonly IMapper _mapper;
 
-    public CommandsController(ICommanderRepo repository, IMapper mapper) {
+    public CommandsController(ICommanderRepo repository, IMapper mapper)
+    {
       _Repository = repository;
       _mapper = mapper;
     }
 
     [HttpGet]
-    public ActionResult<IEnumerable<CommandReadDto>> getAll() {
+    public ActionResult<IEnumerable<CommandReadDto>> getAll()
+    {
       var commands = this._Repository.GetAllCommands();
       return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commands));
     }
 
-    [HttpGet("{id}")]
-    public ActionResult<CommandReadDto> getById(int id) {
+    [HttpGet("{id}", Name="getById")]
+    public ActionResult<CommandReadDto> getById(int id)
+    {
 
       var command = this._Repository.GetCommandById(id);
 
-      if(command == null) {
+      if (command == null)
+      {
         return NotFound();
       }
 
       return Ok(_mapper.Map<CommandReadDto>(command));
+    }
+
+    [HttpPost]
+    public ActionResult<CommandReadDto> CreateCommand(CommandCreateDto cmdCreate)
+    {
+      var commandModel = _mapper.Map<Command>(cmdCreate);
+      _Repository.CreateCommand(commandModel);
+
+      _Repository.saveChanges();
+
+      return CreatedAtRoute(
+        nameof(getById), 
+        new { id = commandModel.Id },
+        commandModel
+      );
     }
   }
 }

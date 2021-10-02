961e1c10d35ac8c6df8a98c827120011b2f24101
Murilo Gonçalves <murilo.gon@live.com>
2021-10-02 11:53:26 -0300

    feat: add entity annotations

--- Models/Command.cs
+++ Models/Command.cs
@@ -1,8 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Commander.Models {
   public class Command {
+    [Key]
     public int Id { get; set; }
+
+    [Required]
+    [MaxLength(200)]
     public string HowTo { get; set; }
+
+    [Required]
+    [MaxLength(200)]
     public string Line { get; set; }
+
+    [Required]
+    [MaxLength(200)]
     public string Plataform { get; set; }
   }
 }
